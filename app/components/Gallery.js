1756c290f9fb90f31bc42b514593407ea75e457b
Jaliel <kangjaliel1998@gmail.com>
2023-06-04 16:39:12 +0700

    gallery-done

--- app/components/Gallery.js
+++ app/components/Gallery.js
@@ -1,45 +1,65 @@
 import { EffectCards } from "swiper";
 import { Swiper, SwiperSlide } from "swiper/react";
+import { qw } from "../fonts";
 import Image from "next/image";
 import "swiper/css";
 import "swiper/css/effect-cards";
 
-
 const Gallery = () => {
+	const images = [
+		{
+			id: 1,
+			name: "campur-0",
+		},
+
+		{
+			id: 2,
+			name: "campur-",
+		},
+		{
+			id: 3,
+			name: "banin-",
+		},
+		{
+			id: 4,
+			name: "banat-",
+		},
+		{
+			id: 5,
+			name: "mdsbareng-",
+		},
+		{
+			id: 6,
+			name: "mds-batik-",
+		},
+	];
 	return (
 		<div className="relative z-20 -mt-[60px] flex items-center flex-col justify-center h-screen overflow-hidden">
+			<h1
+				className={`${qw.className} z-10 text-center text-5xl border-collapse text-green-main`}>
+				Galeri Foto Wisudawan
+			</h1>
 			<Swiper
 				effect={"cards"}
 				grabCursor={true}
 				modules={[EffectCards]}
 				className="mySwiper">
-				<SwiperSlide>
-					<Image src="/img/08.jpg" width={400} height={400} alt="slide" />
-				</SwiperSlide>
-				<SwiperSlide>
-					<Image src="/img/08.jpg" width={400} height={400} alt="slide" />
-				</SwiperSlide>
-				<SwiperSlide>
-					<Image src="/img/08.jpg" width={400} height={400} alt="slide" />
-				</SwiperSlide>
-				<SwiperSlide>
-					<Image src="/img/08.jpg" width={400} height={400} alt="slide" />
-				</SwiperSlide>
-				<SwiperSlide>
-					<Image src="/img/08.jpg" width={400} height={400} alt="slide" />
-				</SwiperSlide>
-				<SwiperSlide>
-					<Image src="/img/08.jpg" width={400} height={400} alt="slide" />
-				</SwiperSlide>
-				<SwiperSlide>
-					<Image src="/img/08.jpg" width={400} height={400} alt="slide" />
-				</SwiperSlide>
-				<SwiperSlide>
-					<Image src="/img/08.jpg" width={400} height={400} alt="slide" />
-				</SwiperSlide>
-				<SwiperSlide>
-					<Image src="/img/08.jpg" width={400} height={400} alt="slide" />
-				</SwiperSlide>
+				{images.map((image) => (
+					<SwiperSlide key={image.id} className="divide-y-8 drop-shadow-lg">
+						<Image
+							src={`/img/${image.name}1.jpg`}
+							width={400}
+							height={400}
+							alt="slide"
+						/>
+						<Image
+							src={`/img/${image.name}2.jpg`}
+							width={400}
+							height={400}
+							alt="slide"
+						/>
+					</SwiperSlide>
+				))}
 			</Swiper>
 		</div>
 	);
